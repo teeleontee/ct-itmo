ebb9c2f47df9183bd8b233591451b82f6a4a0390
teeleontee <leontyevts@gmail.com>
2024-02-28 23:09:04 +0300

    deleted

--- c++/trees-treap/cmake-build-asan/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Leont/OneDrive/Documents/University/cplusplusintro/trees-treap-teeleontee/test
-# Build directory: C:/Users/Leont/OneDrive/Documents/University/cplusplusintro/trees-treap-teeleontee/cmake-build-asan/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- c++/trees-treap/cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Leont/OneDrive/Documents/University/cplusplusintro/trees-treap-teeleontee/googletest
-# Build directory: C:/Users/Leont/OneDrive/Documents/University/cplusplusintro/trees-treap-teeleontee/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
